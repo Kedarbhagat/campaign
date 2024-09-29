3ed4c4dede323628de902834d475fc7a7e51b16f
venkatnarayanan.B <hybridedits2020@gmail.com>
2024-09-29 13:07:11 +0530

    container re usable

---
+++ lib/components/container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Containercustom extends StatelessWidget {
+  const Containercustom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 500, width: 150, child: Image.asset(""));
+  }
+}
